ba088af11f7777e51f84e7b3ce9615eb116dc520
Emma <ainsgreyrat@gmail.com>
2026-04-09 15:14:22 -0500

    api calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import PizzaOfTheDay from "./PizzaOfTheDay";
+import Order from "./Order";
 
 const App = () => {
   return (
-    <div>
-      <h1>Padre Gino's - Order Now</h1>
-      <Pizza
-        name="Pepperoni"
-        description="pep, cheese, n stuff"
-        image={"/public/pizzas/pepperoni.webp"}
-      ></Pizza>
-      <Pizza
-        name="Hawaiian"
-        description="pineapple"
-        image={"/public/pizzas/hawaiian.webp"}
-      ></Pizza>
-      <Pizza
-        name="Americano"
-        description="French fries"
-        image={"/public/pizzas/big_meat.webp"}
-      ></Pizza>
-    </div>
+    <StrictMode>
+      <div>
+        <h1 className="logo">Padre Gino's - Order Now</h1>
+        <Order />
+        <PizzaOfTheDay />
+      </div>
+    </StrictMode>
   );
 };
 
